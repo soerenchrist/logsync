fd8489a1526fc95b8e5158ed5d796fd4b0a2975c
Sören Christ <s.christ@mailbox.org>
2024-03-19 15:21:11 +0100

    Create database query

--- internal/routes/changes.go
+++ internal/routes/changes.go
@@ -3,8 +3,10 @@ package routes
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/soerenchrist/logsync/internal/model"
 	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
 type ChangesController struct {
@@ -21,9 +23,35 @@ func NewChangesController(db *gorm.DB, router *chi.Mux) *ChangesController {
 }
 
 func (c *ChangesController) MapEndpoints() {
-	c.router.Get("/changes", c.getChanges)
+	c.router.Get("/changes/{graphID}", c.getChanges)
 }
 
 func (c *ChangesController) getChanges(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("Changes called")
+	graphId := chi.URLParam(request, "graphID")
+
+	since := request.URL.Query().Get("since")
+	sinceTime, err := parseTime(since)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+
+	var changes []model.ChangeLogEntry
+	tx := c.db.Where("graph_name = ? AND timestamp > ?", graphId, sinceTime).Find(&changes)
+	if tx.Error != nil {
+		fmt.Printf("Error while querying: %v", err)
+		return
+	}
+
+	for i, change := range changes {
+		fmt.Printf("Change %d: %v", i, change)
+	}
+}
+
+func parseTime(since string) (time.Time, error) {
+	if since == "" {
+		return time.UnixMilli(0), nil
+	}
+
+	return time.Parse(time.RFC3339, since)
 }
